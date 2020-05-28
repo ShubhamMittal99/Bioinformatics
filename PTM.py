d3a9590059456ff5835fc14ea12c09fb49e38712
Shubham Mittal <shubhammittal1810@gmail.com>
2020-05-28 23:37:34 +0530

    optimized PTM.py

--- PTM.py
+++ PTM.py
@@ -6,22 +6,6 @@ from Bio import SeqIO
 import csv 
 import os
 
-
-def search_string_in_file(file_name, string_to_search):
-    line_number = 0
-    # Open the file in read only mode
-    with open(file_name, 'r') as read_obj:
-        # Read all lines in the file one by one
-        for line in read_obj:
-            # For each line, check if line contains the string
-            line_number += 1
-            if string_to_search in line:
-                # If yes, then add the line number & line as a tuple in the list
-                result = (line_number, line.rstrip())
- 
-    # Return list of tuples containing line numbers and lines where string is found
-    return result
-
 Output = open("PTM.csv", "w",newline='')
 
 colnames = ["gene name", "amino acid position", "is.modified", "modification.type"]
@@ -30,31 +14,32 @@ colnames = ["gene name", "amino acid position", "is.modified", "modification.typ
 writer = csv.writer(Output)
 writer.writerow(colnames)
 
+string_posit = "var posit"
+string_modif = "var modif"
+
 for seq_record in SeqIO.parse("orf_coding.fasta", "fasta"):
     ID = str(seq_record.id)
+    print(ID)
     URL = 'http://yaam.ifc.unam.mx/detalle_final.php?orf=' + ID
-    temp = open("temporary.txt","w")
+
     try:
         request = requests.get(URL)
     except ConnectionError:
-        print('Web site does not exist:')
-        print(ID + "\n")
+        print(' \n \n Web site does NOT exist: \n\n\n')
     else:
+        print('Web site exists: \n')
         fhand = urllib.request.urlopen(URL)
         for line in fhand:
-            temp.write(line.decode('utf-8').encode('cp850','replace').decode('cp850').strip() + "\n")
-
-        req1 = str(search_string_in_file("temporary.txt", "var posit")) 
-        req1 = req1[req1.find('[') : req1.find(']')+1]
-        positions = ast.literal_eval(req1)
-
-        req2 = str(search_string_in_file("temporary.txt", "var modi")) 
-        req2 = req2[req2.find('[') : req2.find(']')+1]
-        modifications = ast.literal_eval(req2)
-        if positions == []:
-            writer.writerow([ID,str(positions),0,str(modifications)])
-        else:
-            writer.writerow([ID,str(positions),1,str(modifications)])
-    temp.close()
-    os.remove("temporary.txt")
+            if string_posit in  str(line):
+                req1 = (str(line).rstrip())
+                req1 = req1[req1.find('[') : req1.find(']')+1]
+                positions = ast.literal_eval(req1)
+            if string_modif in str(line):
+                req2 = (str(line).rstrip())
+                req2 = req2[req2.find('[') : req2.find(']')+1]
+                modifications = ast.literal_eval(req2)
+                if modifications == []:
+                    writer.writerow([ID,str(positions),0,str(modifications)])
+                else:
+                    writer.writerow([ID,str(positions),1,str(modifications)])
 Output.close()
