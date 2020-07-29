58832d2e655f233d303bedc1d7dd148bfe5e1cf3
Shubham Mittal <shubhammittal1810@gmail.com>
2020-07-29 13:30:10 +0530

    Added homologous genes with threshold as 80% matched

--- Homologous Genes/Blastp.sh
+++ Homologous Genes/Blastp.sh
@@ -1,3 +1,4 @@
 makeblastdb -in GRCh38_latest_protein.faa -out human -dbtype prot -title "Human genome" 
 blastp -query orf_trans.fasta -db human -out Homologous.txt -max_target_seqs 1 -outfmt 6 -num_threads 4
 awk '{print $1 "," $2 "," $3}' Homologous.txt | sort -u > Homologous_tabulated.csv
+sort -k1,1 -k3,3rn -t, Homologous_tabulated.csv | sort -uk1,1 -t, | awk -F"," '$3>80' > Homologous_sorted.csv
